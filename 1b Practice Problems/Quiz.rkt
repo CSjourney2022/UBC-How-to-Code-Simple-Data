d14e3843d9cff79e1fe5fe59258e7d5487acc99c
Eason Hu <110217156+EasonCSjourney@users.noreply.github.com>
2022-08-12 17:10:25 -0700

    Update Quiz.rkt

--- 1b Practice Problems/Quiz.rkt
+++ 1b Practice Problems/Quiz.rkt
@@ -1,26 +1,42 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Quiz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 
 ;; Image Image -> Boolean
-;; produce true if the area of the first iamge is larger than the second
+;; produce true if the first image is larger (height and width are both larger) than the second
+
 (check-expect (first-larger?
                (rectangle 60 30 "outline" "black")
-               (rectangle 67 64 "outline" "black")) false)
+               (rectangle 67 66 "outline" "black")) false)
 (check-expect (first-larger?
-               (ellipse 60 100 "solid" "red")
-               (rectangle 67 64 "outline" "black")) true)
+               (rectangle 77 76 "outline" "black")
+               (rectangle 67 66 "outline" "black")) true)
 (check-expect (first-larger?
                (ellipse 60 100 "solid" "red")
                (ellipse 60 100 "solid" "red")) false)
+(check-expect (first-larger?
+               (rectangle 70 30 "outline" "black")
+               (rectangle 67 66 "outline" "black")) false)
+(check-expect (first-larger?
+               (rectangle 70 66 "outline" "black")
+               (rectangle 67 66 "outline" "black")) false)
+(check-expect (first-larger?
+               (rectangle 67 66 "outline" "black")
+               (rectangle 67 86 "outline" "black")) false)
+(check-expect (first-larger?
+               (rectangle 67 96 "outline" "black")
+               (rectangle 67 86 "outline" "black")) false)
+(check-expect (first-larger?
+               (rectangle 50 76 "outline" "black")
+               (rectangle 67 66 "outline" "black")) false)
+(check-expect (first-larger?
+               (rectangle 50 66 "outline" "black")
+               (rectangle 67 66 "outline" "black")) false)
 
 ;(define (first-larger? img1 img2)
 ;  true)                               ;stub
 
 ;(define (first-larger? img1 img2)
-;  ...img1 img2)                       ;template
+;  ... img1 img2)                       ;template
 
 (define (first-larger? img1 img2)
-  (> (* (image-width img1) (image-height img1))
-     (* (image-width img2) (image-height img2))))                              
+  (and (> (image-width img1) (image-width img2))
+       (> (image-height img1) (image-height img2))))                              

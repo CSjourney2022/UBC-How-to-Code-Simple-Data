8eca00eecbaf5e40ca7040ddea0a2a5fcf2a7ed5
Eason Hu <110217156+EasonCSjourney@users.noreply.github.com>
2022-08-16 15:19:54 -0700

    Update rocket-starter.rkt

--- 2 Practice Problems/rocket-starter.rkt
+++ 2 Practice Problems/rocket-starter.rkt
@@ -1,20 +1,5 @@
-
-;; rocket-starter.rkt
-
 ;; =================
 ;; Data definitions:
-
-; 
-; PROBLEM A:
-; 
-; You are designing a program to track a rocket's journey as it descends 
-; 100 kilometers to Earth. You are only interested in the descent from 
-; 100 kilometers to touchdown. Once the rocket has landed it is done.
-; 
-; Design a data definition to represent the rocket's remaining descent. 
-; Call it RocketDescent.
-; 
-
 
 ;; RocketDescent is one of:
 ;; - Number(0, 100]
@@ -38,18 +23,10 @@
 ;;  - atomic non-distinct: Integer(0, 100]
 ;;  - atomic distinct: false
 
+
+
 ;; =================
 ;; Functions:
-
-; 
-; PROBLEM B:
-; 
-; Design a function that will output the rocket's remaining descent distance 
-; in a short string that can be broadcast on Twitter. 
-; When the descent is over, the message should be "The rocket has landed!".
-; Call your function rocket-descent-to-msg.
-; 
-
 
 ;; RocketDescent -> String
 ;; produce rocket's remaining descent distance in a short string and "The rocket has landed!" whenthe descent is over

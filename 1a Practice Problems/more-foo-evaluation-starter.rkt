e480486aa4951a19d0396e79fb8257a38f83a8ee
Eason Hu <110217156+EasonCSjourney@users.noreply.github.com>
2022-08-16 15:16:53 -0700

    Update more-foo-evaluation-starter.rkt

--- 1a Practice Problems/more-foo-evaluation-starter.rkt
+++ 1a Practice Problems/more-foo-evaluation-starter.rkt
@@ -1,20 +1,4 @@
 
-;; more-foo-evaluation-starter.rkt
-
-; PROBLEM:
-; 
-; Given the following function definition:
-; 
-; (define (foo n)
-;   (* n n))
-; 
-; Write out the step-by-step evaluation of the expression: 
-; 
-; (foo (+ 3 4))
-; 
-; Be sure to show every intermediate evaluation step.
-; 
-
 
 (foo (+ 3 4))
 

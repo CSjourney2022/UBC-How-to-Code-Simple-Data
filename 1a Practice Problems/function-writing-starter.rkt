b92f798cabf0d497a8067dda2dd7a2b478acbe87
Eason Hu <110217156+EasonCSjourney@users.noreply.github.com>
2022-08-16 15:16:25 -0700

    Update function-writing-starter.rkt

--- 1a Practice Problems/function-writing-starter.rkt
+++ 1a Practice Problems/function-writing-starter.rkt
@@ -1,13 +1,3 @@
-
-;; function-writing-starter.rkt
-
-; 
-; PROBLEM:
-; 
-; Write a function that consumes two numbers and produces the larger of the two. 
-; 
-
-
 (define (larger a b)
   (if (> a b)
       a

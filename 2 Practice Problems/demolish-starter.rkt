ad086c880b6fcd1e901dc68a1bb424acf8fb5b40
Eason Hu <110217156+EasonCSjourney@users.noreply.github.com>
2022-08-16 15:19:14 -0700

    Update demolish-starter.rkt

--- 2 Practice Problems/demolish-starter.rkt
+++ 2 Practice Problems/demolish-starter.rkt
@@ -1,20 +1,3 @@
-;; demolish-starter.rkt
-;; =================
-;; Data definitions:
-
-; 
-; PROBLEM A:
-; 
-; You are assigned to develop a system that will classify 
-; buildings in downtown Vancouver based on how old they are. 
-; According to city guidelines, there are three different classification levels:
-; new, old, and heritage.
-; 
-; Design a data definition to represent these classification levels. 
-; Call it BuildingStatus.
-; 
-
-
 ;; BuildingStatus is one of:
 ;; - "new"
 ;; - "old"
@@ -34,18 +17,10 @@
 ;;  - atomic distinct: "old"
 ;;  - atomic distinct: "heritage"
 
+
+
 ;; =================
 ;; Functions:
-
-; 
-; PROBLEM B:
-; 
-; The city wants to demolish all buildings classified as "old". 
-; You are hired to design a function called demolish? 
-; that determines whether a building should be torn down or not.
-; 
-
-
 
 ;; BuildingStatus -> String
 ;; produce "yes" if the building is classified as "old" and to be demolished

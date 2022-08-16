ebab132c37633637fc683658bb209db2cc318d40
Eason Hu <110217156+EasonCSjourney@users.noreply.github.com>
2022-08-16 15:17:47 -0700

    Update boxify-starter.rkt

--- 1b Practice Problems/boxify-starter.rkt
+++ 1b Practice Problems/boxify-starter.rkt
@@ -1,23 +1,5 @@
 (require 2htdp/image)
 
-;; boxify-starter.rkt
-
-; 
-; PROBLEM:
-; 
-; Use the How to Design Functions (HtDF) recipe to design a function that consumes an image, 
-; and appears to put a box around it. Note that you can do this by creating an "outline" 
-; rectangle that is bigger than the image, and then using overlay to put it on top of the image. 
-; For example:
-; 
-; (boxify (ellipse 60 30 "solid" "red")) should produce .
-; 
-; Remember, when we say DESIGN, we mean follow the recipe.
-; 
-; Leave behind commented out versions of the stub and template.
-; 
-
-
 ;;Image -> Image
 ;;Creat an "outline" rectange around an image
 ;(check-expect (boxify (ellipse 60 30 "solid" "red"))
